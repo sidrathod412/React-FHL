f67c7a33a0cf1c35ab23ba358d6c7d7958cb8f24
Shadia Barada <shadiabarada@microsoft.com>
2024-09-17 12:10:27 +0200

    Push navigation to demos

--- src/simple-component/demo/MessageSimple.tsx
+++ src/simple-component/demo/MessageSimple.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Message = () => {
+const MessageSimple = () => {
   // JSX: JavaScript XML
   return <h1>Hello World!</h1>
 }
 
-export default Message
+export default MessageSimple
